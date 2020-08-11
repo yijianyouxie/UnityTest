b84e6d77fb39d0f17c57cc02685da8f27b035075
liuhaibin <liuhaibin@cyou-inc.com>
2020-08-11 20:14:19 +0800

    提交TestUniform

---
+++ TestUniform/Assets/Uniform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Uniform : MonoBehaviour {
+    public Vector4 col = new Vector4();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Shader.SetGlobalVector("_Col", col);
+    }
+}
