ca302f4c09326c99439c0a3790c43dc670b20a5b
yijianyouxie <haibinxiaochenga@163.com>
2021-08-13 09:57:34 +0800

    ComputeShader Demo提交。

---
+++ ComputeShader/Assets/ComputeAdd/VectorAdd.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VectorAdd : MonoBehaviour {
+
+    public ComputeShader calcMeshShader;
+
+    private ComputeBuffer preBuffer;
+    private ComputeBuffer nextBuffer;
+    private ComputeBuffer resultBuffer;
+
+    public Vector3[] array1;
+    public Vector3[] array2;
+    public Vector3[] resultArr;
+
+    public int length = 16;
+    private int kernel;
+
+	// Use this for initialization
+	void Start ()
+    {
+        bool su = SystemInfo.supportsComputeShaders;
+        if (!su)
+        {
+            Debug.LogError("====not supports.");
+            return;
+        }
+        if (null == calcMeshShader)
+        {
+            Debug.LogError("====ComputeShader is null.");
+            return;
+        }
+        array1 = new Vector3[length];
+        array2 = new Vector3[length];
+        for(int i = 0;i<length;i++)
+        {
+            array1[i] = Vector3.one;
+            array2[i] = Vector3.one * 2;
+        }
+
+        resultArr = new Vector3[length];
+
+        InitBuffers();
+
+        kernel = calcMeshShader.FindKernel("CSMain");
+        calcMeshShader.SetBuffer(kernel, "preVertices", preBuffer);
+        calcMeshShader.SetBuffer(kernel, "nextVertices", nextBuffer);
+        calcMeshShader.SetBuffer(kernel, "Result", resultBuffer);
+	}
+
+    private void InitBuffers()
+    {
+        preBuffer = new ComputeBuffer(array1.Length, 3 * 4);
+        preBuffer.SetData(array1);
+
+        nextBuffer = new ComputeBuffer(array2.Length, 3 * 4);
+        nextBuffer.SetData(array2);
+
+        resultBuffer = new ComputeBuffer(resultArr.Length, 3*4);
+        resultBuffer.SetData(resultArr);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        bool su = SystemInfo.supportsComputeShaders;
+        if (!su)
+        {
+            Debug.LogError("====not supports.");
+            return;
+        }
+        if (null == calcMeshShader)
+        {
+            Debug.LogError("====ComputeShader is null.");
+            return;
+        }
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+            calcMeshShader.Dispatch(kernel, 2, 2, 1);
+            resultBuffer.GetData(resultArr);
+            Debug.LogError("====" + resultArr[0]);
+
+            //resultBuffer.Release();
+        //}
+
+	}
+
+    private string str = "";
+    private void OnGUI()
+    {
+        str = GUI.TextField(new Rect(0, 0, 100, 30), resultArr[0].ToString());
+    }
+
+    private void OnDestroy()
+    {
+        preBuffer.Release();
+        nextBuffer.Release();
+        resultBuffer.Release();
+    }
+}
---
+++ ComputeShader/Assets/ComputeRT/ComputeRT.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComputeRT : MonoBehaviour {
+
+    public ComputeShader computeShader;
+    public Material panelMat;
+
+    private RenderTexture rt;
+    private int kernel;
+
+    public Color color;
+
+    public Color[] colors;
+    private Vector4[] colorVectors;
+
+	// Use this for initialization
+	void Start () {
+
+        bool su = SystemInfo.supportsComputeShaders;
+        if (!su)
+        {
+            Debug.LogError("====not supports.");
+            return;
+        }
+        if (null == computeShader)
+        {
+            Debug.LogError("====ComputeShader is null.");
+            return;
+        }
+
+        if(null == panelMat)
+        {
+            Debug.LogError("====panelMat is null.");
+            return;
+        }
+        kernel = computeShader.FindKernel("CSMain");
+
+        //rt = RenderTexture.GetTemporary(512, 512, 24);
+        rt = new RenderTexture(512, 512, 24);
+        rt.enableRandomWrite = true;
+        rt.Create();
+        //computeShader.SetTexture(kernel, "Result", rt);
+        //computeShader.Dispatch(kernel, 32, 32, 1);
+
+        //panelMat.mainTexture = rt;
+        //转换color2vector
+        int len = colors.Length;
+        colorVectors = new Vector4[len];
+        for(int i = 0;i<len;i++)
+        {
+            colorVectors[i] = new Vector4(colors[i].r, colors[i].g, colors[i].b, colors[i].a);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        bool su = SystemInfo.supportsComputeShaders;
+        if (!su)
+        {
+            Debug.LogError("====not supports.");
+            return;
+        }
+        computeShader.SetVector("color", color);
+        computeShader.SetFloat("time", Time.time);
+        computeShader.SetVectorArray("colors", colorVectors);
+        computeShader.SetTexture(kernel, "Result", rt);
+        computeShader.Dispatch(kernel, 32, 32, 1);
+
+        panelMat.mainTexture = rt;
+    }
+
+    private void OnDestroy()
+    {
+        if(null != panelMat)
+        {
+            panelMat.mainTexture = null;
+        }
+        if( null != rt)
+        {
+            DestroyImmediate(rt);
+        }
+    }
+}
